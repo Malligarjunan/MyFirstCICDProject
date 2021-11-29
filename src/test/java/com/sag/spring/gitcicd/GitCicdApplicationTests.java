48f310d9c434f5e7520dadee700dcb834f66047e
malli <malli@softwareag.com>
2021-11-29 12:00:23 +0530

    "Test Added"

--- src/test/java/com/sag/spring/gitcicd/GitCicdApplicationTests.java
+++ src/test/java/com/sag/spring/gitcicd/GitCicdApplicationTests.java
@@ -2,12 +2,15 @@ package com.sag.spring.gitcicd;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.util.Assert;
 
 @SpringBootTest
 class GitCicdApplicationTests {
 
     @Test
     void contextLoads() {
+        boolean isTrue = false;
+        Assert.isTrue(isTrue, "It is not true");
     }
 
 }
